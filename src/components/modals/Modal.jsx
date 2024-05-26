800bf861e20aae196d981dad1cff3789dcf23923
elchanan <you@example.com>
2024-05-26 10:17:17 +0300

    add dark mode, card modal

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import { IoIosCloseCircleOutline } from "react-icons/io";
+import ProductPage from '../product/productTable/ProductPage';
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;  // אם isOpen הוא false, המודאל לא יוצג
-
+  
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="relative bg-white p-5 rounded shadow-lg w-full max-w-2xl mx-auto">
-        <button className="absolute top-2 right-2 text-xl" onClick={onClose}>x</button>
+        <button className="absolute top-2 right-2 text-xl hover:bg-blue-800" onClick={onClose}><IoIosCloseCircleOutline /></button>
         {children}
       </div>
     </div>
